39b452d861a250f1dde8ee8ff039d9e29f5ce03f
Luke <luke@agilelite.com>
2022-09-18 11:03:15 +0200

    Restructure the repo, resolve compilation issues, setup tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import Header  from "../components/Header/Header";
+import Header from "../components/Header/Header";
+import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Goerli;
